79a8a4c7f05618f618c9b2a17d07c24005dbdf75
DimWitRM <DimWitRM@gmail.com>
2019-11-28 20:13:28 -0500

    Added books display, book details, update and delete

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const mainRoute = require('./routes');
-
+const bodyParser = require('body-parser')
 const app = express();
 
+
+// view engine setup
 app.set('view engine', 'pug');
+
 app.use('/static', express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(mainRoute);
 
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,42 @@ const express = require('express');
 const router = express.Router();
 const {Books} = require('../db').models;
 
+
+// Display all the books
 router.get('/', async (req,res) => {
     const books = await Books.findAll();
-    console.log(books);
-    console.log(books.Book)
-    console.log(books.length);
-    res.render('books/index', books);
+    res.render('books/index', { books });
+});
+
+// Display the create book section
+router.get('/create', async(req,res) => {
+    res.render('books/new-book');
+});
+
+// Create the new book
+router.post('/', async(req,res) => {
+    const book = await Books.create(req.body);
+    res.redirect('/');
+});
+
+// Display the details of the book
+router.get('/books/:id', async (req,res) => {
+    const book = await Books.findByPk(req.params.id);
+    res.render('books/update-book', { book });
+});
+
+// Updates the details of the book and display all books
+router.post('/books/:id', async (req,res) => {
+    const book = await Books.findByPk(req.params.id);
+    await book.update(req.body);
+    res.redirect('/');
+});
+
+// Delete the book from the books
+router.post('/books/:id/delete', async (req,res) => {
+    const book = await Books.findByPk(req.params.id);
+    await book.destroy(req.body);
+    res.redirect('/');
 });
 
 module.exports = router;
